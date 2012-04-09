0485bad57fcf49496c1c0a01ab36140b24753c3c
William Ferraguti <homer.dizamok@laposte.net>
2012-04-09 19:19:47 +0200

    #6 Oublie du fichier algo

---
+++ WSEDT/grails-app/services/wsedt/Algo/Algo.groovy
@@ -0,0 +1,66 @@
+package wsedt.Algo
+
+import java.util.ArrayList;
+
+import wsedt.Reservation;
+import wsedt.Salle;
+
+abstract class Algo {
+	
+	static ArrayList<Salle> rechercherSalles(int annee, int mois, int jour, int heure, int minute, String batiment, int capaciteMin){
+		//+ filtrer les salles
+		//+ int jour int heure
+		
+	   Reservation arg = new Reservation(annee: annee, mois: mois, jour: jour, heure: heure, minute: minute)
+	   ArrayList<Reservation> reservations = Reservation.getAll()
+	   //System.out.println("Liste des reservations : " + reservations)
+	   ArrayList<Salle> sallesNonLibres = new ArrayList<Salle>()
+	   
+		//Trouver toutes les salles non disponibles
+		for(Reservation debutReservation : reservations){
+			Salle s = debutReservation.getSalle()
+			
+			if(!sallesNonLibres.contains(s)){
+			   Reservation finCours = debutReservation.getFinReservation()
+			  // System.out.println("Fin de la reservation : " + finCours)
+			   
+			   //Si la salle n'est pas libre (ie. en même temps ou entre le cours)
+				if(debutReservation.enMemeTemps(arg) || (arg.estApres(debutReservation) && arg.estAvant(finCours))){
+					//System.out.println("La salle " + s + " n'est pas libre")
+					sallesNonLibres.add(s)
+				}
+			}
+		}
+	   
+		//Les autres salles sont celles disponibles
+		ArrayList<Salle> salles = Salle.getAll()
+		ArrayList<Salle> sallesLibres = new ArrayList<Salle>()
+		
+		for(Salle s : salles){
+			if(!sallesNonLibres.contains(s))
+				if(batiment.isEmpty() || s.getBatiment().equals(batiment)){ //On vérifie si le batiment de la salle est celui demandé
+					if(capaciteMin == 0 || s.getCapacite() >= capaciteMin) //On vérifie si la capacité minimum de la salle est celle demandée
+						sallesLibres.add(s)
+				}
+		}
+		
+		return sallesLibres
+	}
+	
+	static ArrayList<Salle> rechercherSalles(int annee, int mois, int jour, int heure, int minute){
+		return rechercherSalles(annee, mois, jour, heure, minute, new String(""), 0)
+	}
+	
+	
+	 static ArrayList<Salle> rechercherSalles(int annee, int mois, int jour, int heure, int minute, String batiment){
+		return rechercherSalles(annee, mois, jour, heure, minute, batiment, 0)
+	}
+	
+	 static ArrayList<Salle> rechercherSalles(int annee, int mois, int jour, int heure, int minute, int capaciteMin){
+		return rechercherSalles(annee, mois, jour, heure, minute, new String(""), capaciteMin)
+	}
+	
+	
+
+
+}
